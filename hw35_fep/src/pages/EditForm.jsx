483d6c35622441960e0f0cdebd9efbc08d35d32c
doxliydriw <123301390+doxliydriw@users.noreply.github.com>
2023-09-29 18:19:00 +0200

    test commit

---
+++ hw35_fep/src/pages/EditForm.jsx
@@ -0,0 +1,61 @@
+import { useFormik } from 'formik';
+import { React, useState } from 'react';
+import { NavLink, useNavigate, useLocation } from 'react-router-dom';
+import EntryList from './EntryLlst';
+ 
+function EditForm(editEntry) {
+    let entry = useLocation().state;
+    const navigate = useNavigate();
+    const formik = useFormik({
+     initialValues: {
+                    name: entry.name,
+                    phone: entry.phone,
+                    id: entry.id,
+    },
+        onSubmit: (values) => {
+        //  console.log(values)
+            if (values.name && values.phone) {
+                                        editEntry(values);
+                                        // props.addEntry(values);
+                                        navigate('/');
+        } else {
+         alert('Please enter all mandatory data');
+        }
+     },
+   });
+    return (
+         <>
+            <h1>Edit entry {entry.name}</h1>
+            <form onSubmit={formik.handleSubmit}>
+            <div className='card_cntr'>
+              <ul className="card">
+                    <li>
+                          <label htmlFor="firstName">Name</label>
+                          <input
+                              id="name"
+                              name="name"
+                              type="text"
+                              onChange={formik.handleChange}
+                              value={formik.values.name}
+                          />
+                    </li>
+                    <li>
+                          <label htmlFor="email">Phone number</label>
+                          <input
+                              id="phone"
+                              name="phone"
+                              type="phone"
+                              onChange={formik.handleChange}
+                              value={formik.values.phone}
+                          />
+                    </li>
+                </ul>
+          </div>
+          <button className="submit" type="submit">Submit</button>
+          <button className='cancel'><NavLink to='/'>cancel</NavLink></button>
+        </form>
+        </>
+   );
+};
+
+export default EditForm
